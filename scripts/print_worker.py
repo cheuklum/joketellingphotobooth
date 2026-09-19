8afaa80e2672dfcda901801a61f43714eb895a3a
Stephen Mock <sjmock99@gmail.com>
2026-09-19 12:02:47 -0700

    Improvement on dithering technique for actual printing

--- scripts/print_worker.py
+++ scripts/print_worker.py
@@ -1,4 +1,3 @@
-/project1/freeze_cache_5# Inside a Text DAT named 'print_worker'
 import os
 import glob
 from datetime import datetime
--- scripts/trigger_action.py
+++ scripts/trigger_action.py
@@ -5,7 +5,8 @@ import subprocess
 import random
 import textwrap
 
-PRINTER_NAME = "Munbyn RW403B-N(Bluetooth)" 
+# PRINTER_NAME = "Munbyn RW403B-N(Bluetooth)" 
+PRINTER_NAME = "Munbyn RW403B-N" 
 TEXT_FILE_PATH = os.path.normpath(os.path.join(project.folder, 'text.txt'))
 WRAP_WIDTH = 35            # max characters per line; longer joke lines wrap instead of shrinking
 
